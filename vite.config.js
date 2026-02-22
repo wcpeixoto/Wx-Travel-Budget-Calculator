b9c874dc6fa1876cb4db84f4a6f1b28e21b15f47
wcpeixoto <62615809+wcpeixoto@users.noreply.github.com>
2026-02-22 07:37:32 -0500

    fix: set vite js config base to root for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig(function (_a) {
-    var command = _a.command;
-    return ({
-        plugins: [react()],
-        base: command === 'serve' ? '/' : '/Wx-Travel-Budget-Calculator/',
-    });
+export default defineConfig({
+    plugins: [react()],
+    base: '/',
 });
